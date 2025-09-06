f9cc538c0228c50c1a838dc7fb6bbea94d5e14b2
Loneaabis <arilone45@gmail.com>
2025-09-06 19:03:12 +0530

    Configure for Vercel HTTPS deployment

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    API_BASE_URL: 'http://162.250.191.81:3002',
-    SOCKET_URL: 'http://162.250.191.81:3002',
+    API_BASE_URL: 'https://seismicwall.duckdns.org/renaissance',
+    SOCKET_URL: 'https://seismicwall.duckdns.org/renaissance',
     
     GAME_VERSION: '1.0.0',
     MAX_PLAYERS_PER_GAME: 2,
@@ -9,3 +9,5 @@ const CONFIG = {
     
     DEBUG: true
 };
+
+console.log('Renaissance Config Loaded:', CONFIG);
